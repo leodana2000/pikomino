b21aaa85f1930d364b8ca440aec5bd0873fb5822
Léo Dana <cargodana8@gmail.com>
2023-10-22 15:26:31 +0200

    init files

--- pikomino-1-turn.py
+++ pikomino-1-turn.py
@@ -3,3 +3,9 @@ Implements optimal strategy for pikomino in one turn.
 We use dynamic programming to find the optimal solution.
 '''
 
+# Action space
+class Action:
+    SUM : int
+    IS_WORMS : bool
+    PREVIOUS_DICE : list[int]
+
